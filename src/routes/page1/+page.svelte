e748131f66bcbab8c0f0f9e6f263f0a50645d0b4
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-05-30 08:31:13 +0200

    .

--- src/routes/page1/+page.svelte
+++ src/routes/page1/+page.svelte
@@ -1,9 +1,15 @@
 <script>
     import Comp1 from "$lib/Comp1.svelte";
+    var props
 </script>
 
 <h1>1. oldal</h1>
-<div class="c1c"><Comp1 n=2/></div>
+{#each Array(4).fill() as _, i}
+    { props = {n: Number(i + 2), x: Number(i + 2)}, '' }
+    <div class="c1c">
+        <Comp1 {...props} />
+    </div>
+{/each}
 <style>
     div.c1c {
         text-align: center;
